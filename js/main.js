a102140f753bdd674a06f86905c98105fa9e4252
rlaecio <rlaecio@hotmail.com>
2023-03-16 15:33:53 +0000

    refactory 2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-const subtrair = document.querySelector("#subtrair");
-const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco");
 
 const controle = document.querySelectorAll(".controle-ajuste")
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        console.log(evento.target.textContent)
+        manipulaDados(evento.target.textContent, evento.target.parentNode)        
     })
 })
 
-somar.addEventListener("click", () => {manipulaDados("somar")});
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")});
-
-function manipulaDados(operacao) {
-    if(operacao === "subtrair"){
-        braco.value = parseInt(braco.value) -1;        
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector(".controle-contador")
+    if(operacao === "-"){
+        peca.value = parseInt(braco.value) -1;        
     } else {
-        braco.value = parseInt(braco.value) +1;
+        peca.value = parseInt(braco.value) +1;
     }
 }
